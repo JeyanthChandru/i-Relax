40726820f94f5791eafa239597acf73ef3997aa5
Nirmal Kumar Ravichandran <nirmalkumar.r12@gmail.com>
2017-10-02 19:20:48 -0500

    first Game Idea
    Game to destroy objects using a mouse click

---
+++ Gamesample/Assets/CubeBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class CubeBehaviour : MonoBehaviour {
+    public static int score=0;
+    public Text text123;
+    void OnMouseDown()
+    {
+        Destroy(gameObject);
+        score += 50;
+
+    }
+    void Update()
+    {
+        text123.text = ""+score.ToString();
+    }
+    void Start()
+    {
+        text123.text=""+score.ToString();
+    
+    }
+}
