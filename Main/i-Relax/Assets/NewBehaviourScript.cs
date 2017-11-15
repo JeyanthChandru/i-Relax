7940991ef5bf16246fbd4fbf2f6d1d4878db63da
Nirmal Kumar Ravichandran <nirmalkumar.r12@gmail.com>
2017-11-15 10:01:57 -0600

    Added AppScene and OptionsScene

---
+++ Main/i-Relax/Assets/NewBehaviourScript.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.SceneManagement;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hwnd, int nCmdShow);
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    bool sta = true,ca=false;
+	// Use this for initialization
+	void Start () {
+        Application.runInBackground = true;
+        Screen.fullScreen = !Screen.fullScreen;
+        ShowWindow(GetActiveWindow(), 2);
+        startAfterTime();
+        
+	}
+    void startAfterTime()
+    {
+
+        Thread.Sleep(10000);
+      //sta = false;
+        SceneManager.LoadScene("OptionsScene");
+      ca = true;
+
+
+    }
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Application.runInBackground = true;
+        if (ca)
+        {
+            sta = false;
+            //Application.runInBackground = false;
+            
+            //ShowWindow(GetActiveWindow(), 2);
+            
+
+        }
+    }
+}
