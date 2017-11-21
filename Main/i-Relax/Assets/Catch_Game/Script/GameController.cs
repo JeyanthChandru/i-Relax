53396aecb7375a1bbb774049e73dbd7c5f44caa5
MoneyABCs <utaflpmoneyabcs@gmail.com>
2017-11-21 13:57:44 -0600

    Catch Game
    Catch Game

---
+++ Main/i-Relax/Assets/Catch_Game/Script/GameController.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject[] balls;
+    public float timeLeft;
+    private float maxWidth;
+    private bool counting;
+    public Text timerText;
+    public GameObject gameOverText;
+    public GameObject restartButton;
+    //public GameObject splashScreen;
+    public GameObject startButton;
+    public Bin_controller bincontroller;
+
+    void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float ballWidth = balls[0].GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - ballWidth;
+        timerText.text = "TIME LEFT:\n" + Mathf.RoundToInt(timeLeft);
+    }
+
+    public void StartGame()
+    {
+        print("Game Started");
+        //splashScreen.SetActive(false);
+        startButton.SetActive(false);
+        bincontroller.ToggleControl(true);
+        StartCoroutine(Spawn());
+    }
+
+    void FixedUpdate()
+    {
+        if (counting)
+        {
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+                timeLeft = 0;
+            }
+            timerText.text = "TIME LEFT:\n" + Mathf.RoundToInt(timeLeft);
+        }
+    }
+
+
+    public IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        counting = true;
+        while (timeLeft > 0)
+        {
+            GameObject ball = balls[Random.Range(0, balls.Length)];
+            Vector3 spawnPosition = new Vector3(
+                transform.position.x + Random.Range(-maxWidth, maxWidth),
+                transform.position.y,
+                0.0f
+            );
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(ball, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        }
+        yield return new WaitForSeconds(2.0f);
+        gameOverText.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        restartButton.SetActive(true);
+    }
+}
---
+++ Main/i-Relax/Assets/Catch_Game/Script/Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    public Text scoreText;
+    public int ballValue;
+
+    private int score;
+
+    void Start()
+    {
+        score = 0;
+        UpdateScore();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        score += ballValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "SCORE:\n" + score;
+    }
+}
