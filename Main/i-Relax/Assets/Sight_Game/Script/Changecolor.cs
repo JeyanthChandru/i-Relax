5e08fa4e4e9ca00e8b957462882e7885476c10a0
Dhinesh Kumar Sivakumaran <dhineshkumar.s89@gmail.com>
2017-11-23 14:41:49 -0600

    Sight Game

---
+++ Main/i-Relax/Assets/Sight_Game/Script/Changecolor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Changecolor : MonoBehaviour {
+    public Color startColor;
+    public Color mouseOverColor;
+    public Renderer rend;
+    bool mouseOver = false;
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+    void OnMouseEnter()
+    {
+        mouseOver = true;
+        rend.material.SetColor("_Color", mouseOverColor);
+    }
+    void OnMouseOver()
+    {
+        print("over");
+        rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+    }
+    void OnMouseExit()
+    {
+        mouseOver = false;
+        rend.material.SetColor("_Color", startColor);
+    }
+}
