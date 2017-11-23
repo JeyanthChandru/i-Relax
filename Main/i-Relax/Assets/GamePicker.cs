43da9c976531bce520fabd5c6d0b6efdd214b85d
unknown <nirmalkumar.r12@gmail.com>
2017-11-23 13:43:16 -0600

    Made changes to Options scene BrickBreaker

---
+++ Main/i-Relax/Assets/GamePicker.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamePicker : MonoBehaviour {
+
+	// Logic for what happens when Play button is clicked.
+	public void PickScene(){
+		string[] name = new string[] {
+		"CatchScene",
+			"BreakGameStart"};
+		System.Random rand = new System.Random ();
+		int randomNumber =rand.Next (1, 3);
+		string pickedScene = name [1];
+		SceneManager.LoadScene (pickedScene);
+	
+	
+	
+	
+	
+	}
+	// Logic for what happens when Skip button is clicked.
+	public void SkipGame(){
+	}
+
+	//Logic for what happens when Settings button is clicked.
+	public void ShowSettings(){
+	
+	
+	
+	}
+
+}
---
+++ Main/i-Relax/Assets/GameTimer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameTimer : MonoBehaviour {
+	public Text Timer;
+float timeLeft = 20.0f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeLeft -= Time.deltaTime;
+		Timer.text = "Time Left:"+(int)timeLeft;
+		if(timeLeft < 0)
+		{
+			
+			Application.Quit();
+		}
+		
+	}
+}
